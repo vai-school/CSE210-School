1afed7e70a6ec2c8c7df2ef4fd3d308438189418
vai-school <vai22005@byui.edu>
2026-01-08 11:23:18 -0700

    Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your First Name?: ");
+        string First = Console.ReadLine();
+        Console.Write("What is your Last Name?: ");
+        string Last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {Last}, {First} {Last}");
     }
 }
