e8e8614e2d50fc51056faf5f5632dd43bc9826bc
vai-school <vai22005@byui.edu>
2026-01-08 11:37:20 -0700

    Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade?: ");
+        int grade = int.Parse(Console.ReadLine());
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("You have an A");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("You have a B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("You have a C");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("You have a D");
+        }
+        else
+        {
+            Console.WriteLine("You have a F");
+        }
     }
 }
