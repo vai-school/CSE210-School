54e7b266d242b2160f2e288fc8c2840c61452f7f
vai-school <vai22005@byui.edu>
2026-01-14 11:05:04 -0700

    Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int magic;
+        int guess;
+
+    
+
+        Console.Write("What's the magic number? ");
+        magic = int.Parse(Console.ReadLine());
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > magic)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else if (guess < magic)
+            {
+                Console.WriteLine("Too low!");
+            }
+            else
+            {
+                Console.WriteLine("Correct!");
+            }
+
+        } while (guess != magic);
+
+    
     }
 }
