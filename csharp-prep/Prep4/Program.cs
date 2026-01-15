bdb510adf0c452d8c36386ecbc7d4b52acf45f9f
vai-school <vai22005@byui.edu>
2026-01-15 14:30:18 -0700

    Update Program.cs

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int number;
+        List<int> numbers = new List<int>();
+
+        do
+        {
+            Console.Write("Type a number (0 to quit): ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+
+        } while (number != 0);
+
+        int sum = 0;
+        foreach (int i in numbers)
+        {
+            sum += i;
+        }
+        
+        double average = (double)sum / numbers.Count;
+
+        int max = numbers[0];
+        foreach (int i in numbers)
+        {
+            if (i > max)
+            {
+                max = i;
+            }
+        }
+
+        Console.WriteLine($"Sum: {sum}");
+        Console.WriteLine($"Average: {average}");
+        Console.WriteLine($"Max: {max}");
     }
+
 }
