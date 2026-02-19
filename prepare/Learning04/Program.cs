4a7e26842549d636f608533eb15f190c93cbc278
vai-school <vai22005@byui.edu>
2026-02-19 13:16:13 -0700

    stuff

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Michael", "Times Tables");
+        Console.WriteLine(a1.GetSummary());
+        MathAssignment a2 = new MathAssignment("Carson", "Long Division", "1", "1-3");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+        WritingAssignment a3 = new WritingAssignment("John", "WW2", "How war affected Europe");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
